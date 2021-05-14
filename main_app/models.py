cf9681ee0f1f4099ecb133eb7c3e1e64ad032385
mattius9 <matthew.krasucki@gmail.com>
2021-05-14 02:41:41 -0400

    migrate mineral schema and add create superuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Mineral(models.Model):
+    name = models.CharField(max_length=100)
+    m_class = models.CharField(max_length=100)
+    colour = models.CharField(max_length=100)
+    hardness = models.FloatField(validators=[MinValueValidator(0.0),MaxValueValidator(10.0)])
+    magnetic = models.BooleanField(default=False)
--- main_app/views.py
+++ main_app/views.py
@@ -1,30 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Mineral
+
 # Create your views here.
 
 # Temporary Dataset
 
-class Mineral:
-    def __init__(self,name,m_class,colour,hardness,magnetic=False):
-        self.name = name
-        self.m_class = m_class
-        self.colour = colour
-        self.hardness = hardness
-        self.magnetic = magnetic
+# class Mineral:
+#     def __init__(self,name,m_class,colour,hardness,magnetic=False):
+#         self.name = name
+#         self.m_class = m_class
+#         self.colour = colour
+#         self.hardness = hardness
+#         self.magnetic = magnetic
 
-minerals = [
-    Mineral('Amethyst', 'tectosilicate', 'violet', 7 ),
-    Mineral('Pyrite', 'sulfide', 'brass', 6),
-    Mineral('Cinnabar', 'sulfide', 'brown-red', 2),
-    Mineral('Chromite', 'oxide', 'brown-black',5.5 ,True)
-]
+# minerals = [
+#     Mineral('Amethyst', 'tectosilicate', 'violet', 7 ),
+#     Mineral('Pyrite', 'sulfide', 'brass', 6),
+#     Mineral('Cinnabar', 'sulfide', 'brown-red', 2),
+#     Mineral('Chromite', 'oxide', 'brown-black',5.5 ,True)
+# ]
 
 
 def home(request):
-    return HttpResponse('<h1>Hello ¯\_( ツ )_/¯ <h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def minerals_index(request):
+    minerals = Mineral.objects.all()
     return render(request, 'minerals/index.html', {'minerals': minerals})
